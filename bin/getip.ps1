5a1040f0b90aa4d307494b76055380d8dabd0573
ChandraKamarapu <chandrakamarapu@outlook.com>
2017-10-26 12:15:45 +0530

    Run Automation

--- bin/getip.ps1
+++ bin/getip.ps1
@@ -1,3 +1,4 @@
 C:\concourse\AzureAuthenticator.ps1
-Get-AzureRMStorageUsage
-Get-AzureRMVmUsage -Location WestUS2 | Format-Table
+cd C:\concourse\azure-linux-automation-master
+
+.\AzureAutomationManager.ps1 -xmlConfigFile .\Azure_ICA_all_run-RHEL67.xml -runtests -Distro CKR74 -cycleName PROVISION -UseAzureResourceManager -keepReproInact
