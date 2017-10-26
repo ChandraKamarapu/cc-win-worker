7bf354fa4bcb84e3186cef8b4aa0df13ecf0c6fc
ChandraKamarapu <chandrakamarapu@outlook.com>
2017-10-26 15:43:29 +0530

    Update getip.ps1

--- bin/getip.ps1
+++ bin/getip.ps1
@@ -1,4 +1,1 @@
-C:\concourse\AzureAuthenticator.ps1
-cd C:\concourse\azure-linux-automation-master
-
-.\AzureAutomationManager.ps1 -xmlConfigFile .\Azure_ICA_all_run-RHEL67.xml -runtests -Distro CKR74 -cycleName PROVISION -UseAzureResourceManager -keepReproInact
+Get-Process
